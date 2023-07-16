faca36b32d8dc26fbf44101381891662606c70b1
David Groomes <dwgroomes@gmail.com>
2023-07-16 14:55:37 -0500

    Implement a working example program

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # tomcat-playground
 
-NOT YET IMPLEMENTED
-
 📚 Learning and exploring Apache Tomcat.
 
-> Apache Tomcat
+> ### Apache Tomcat
 >
 > The Apache Tomcat® software is an open source implementation of the Jakarta Servlet, Jakarta Server Pages, Jakarta
 > Expression Language, Jakarta WebSocket, Jakarta Annotations and Jakarta Authentication specifications. These
@@ -22,28 +20,37 @@ project is a place for me to do that.
 
 This might turn into more of a servlet playground than a Tomcat playground. We'll see.
 
+**NOTE**: This project was developed on macOS. It is for my own personal use.
+
 
 ## Instructions
 
 Follow these instructions to build and run the example program.
 
 1. Use Java 17
-2. Run the app
+2. Start the demo program and web server
     * NOT YET IMPLEMENTED
     * ```shell
       ./gradlew run
       ```
+3. Open the browser
+    * Let's see the final effect by opening the browser to <http://localhost:8080/message>. You should see a special
+      message from the server.
+4. Stop the server
+    * When you're ready, stop the demo program and server with the `Ctrl+C` key combination.
 
 
 ## Wish List
 
 General clean-ups, TODOs and things I wish to implement for this project:
 
-* [ ] IN PROGRESS Implement the project. Do the minimum to run an embedded Tomcat server (I'm not very interested in
+* [x] DONE Implement the project. Do the minimum to run an embedded Tomcat server (I'm not very interested in
   a non-embedded Tomcat server at this time. I'm not sure that I'm using the right words).
+* [ ] Clean up the logging. Is Tomcat logging via SLF4J?
 * [ ] Consider exploring servlets just a little bit
 * [ ] Consider exploring Tomcat's support for Java virtual threads
-
+* [ ] Serve a static file (we love static content, let's have a working example).
+* [ ] Set the file stuff to a temp directory or something. I'm seeing a `tomcat.8080` directory in the project root.
 
 ## Reference
 
--- src/main/java/dgroomes/Main.java
+++ src/main/java/dgroomes/Main.java
@@ -1,16 +1,65 @@
 package dgroomes;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * Please see the README for more information.
  */
 public class Main {
 
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    static final Logger log = LoggerFactory.getLogger(Main.class);
+    Tomcat tomcat;
+    Context ctx;
 
-    public static void main(String[] args) {
-        log.info("Not yet implemented");
+    public static void main(String[] args) throws Exception {
+        log.info("Let's learn about Apache Tomcat by running an embedded server");
+        new Main().run();
+    }
+
+    void run() throws Exception {
+        initTomcatBoilerplate();
+
+        // Add our custom servlet to the container
+        Tomcat.addServlet(ctx, "messageServlet", new MessageServlet());
+        ctx.addServletMappingDecoded("/message", "messageServlet");
+
+        // Start the server and make the main thread wait till the server is stopped
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    /**
+     * Initialize a {@link Tomcat} instance along with the necessary minimal boilerplate. Part of the boilerplate is
+     * the creation of a {@link Context} instance.
+     */
+    void initTomcatBoilerplate() {
+        tomcat = new Tomcat();
+
+        // As with any server, we need to define a port to listen on.
+        tomcat.setPort(8080);
+
+        // "Context" is a concept in Tomcat. It's some kind of "container". It's a bit abstract to me. I haven't learned
+        // much about it yet. Can we do something with that second parameter "docBase"?
+        ctx = tomcat.addContext("", null);
+
+        // This is an odd but required step. See https://stackoverflow.com/a/59282431
+        tomcat.getConnector();
+    }
+}
+
+class MessageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Hello from 'tomcat-playground'!");
     }
 }
